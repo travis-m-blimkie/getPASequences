c3c2412021c977072daf3bf1bc405978146faf74
Travis <travis.m.blimkie@gmail.com>
2019-08-13 10:52:22 -0700

    Now able to download nucleotide and amino acid sequences, along with annotation file.

--- app.R
+++ app.R
@@ -1,7 +1,5 @@
 
-# TODO Add download information in ui and server sections
 # TODO Add message if strain and IDs don't match (i.e. 0 rows in table)
-# TODO disable download buttons until data is ready
 
 
 # Load libraries and data -------------------------------------------------
@@ -9,6 +7,7 @@
 library(shiny)
 library(shinythemes)
 library(shinyjs)
+library(seqinr)
 library(tidyverse)
 
 pao1Data <- readRDS("data/Pseudomonas_aeruginosa_PAO1_107.Rds")
@@ -42,7 +41,7 @@ ui <- fluidPage(
                 c("PAO1" = "PAO1",
                   "PA14" = "PA14",
                   "LESB58" = "LESB58")
-                ),
+            ),
 
 
             # Place to paste your genes of interest
@@ -50,7 +49,7 @@ ui <- fluidPage(
                 "pastedInput",
                 "Paste your list of locus tags, one per line:",
                 height = "300px"
-                ),
+            ),
 
 
             # Button which triggers results to display. "background-color"
@@ -60,15 +59,32 @@ ui <- fluidPage(
                 label   = "Search",
                 icon    = icon("search"),
                 style   = "color: #fff; background-color: #18bc9c; border-color: #18bc9c"
-                ),
+            ),
 
             tags$br(),
             tags$br(),
 
+            # Download button for annotation table
             disabled(downloadButton(
                 "resultTable",
                 "Download Annotations",
-                style = "color: #fff; background-color: #337ab7; border-color: #337ab7"))
+                style = "color: #fff; background-color: #337ab7; border-color: #337ab7"
+            )),
+
+            tags$br(),
+            tags$br(),
+
+            # Download button for nucleotide sequences
+            disabled(downloadButton(
+                "ntSeqs",
+                "Nucleotide Sequences"
+            )),
+
+            # Download button for amino acid sequences
+            disabled(downloadButton(
+                "aaSeqs",
+                "Protein Sequences"
+            ))
         ),
 
         mainPanel(
@@ -155,7 +171,7 @@ server <- function(input, output) {
         }, options = list(searching = FALSE))
 
 
-        # Download button for displayTable
+        # Download button for displayTable, enabled and then populated
         enable("resultTable")
         output$resultTable <- downloadHandler(
             filename = function() {
@@ -164,6 +180,38 @@ server <- function(input, output) {
             content = function(file) {
                 write.csv(displayTable(), file, row.names = FALSE, quote = FALSE)
             })
+
+
+        # Download data for nucleotide sequences i.e. ntSeqs
+        enable("ntSeqs")
+        output$ntSeqs <- downloadHandler(
+            filename = function() {
+                paste0(input$strainChoice, "_nucleotideSequences.fasta")
+            },
+            content = function(file) {
+                write.fasta(
+                    as.list(filteredTable()$Nucleotide_Sequence),
+                    names = filteredTable()$Locus_Tag,
+                    file.out = file
+                    )
+            }
+        )
+
+
+        # Download data for amino acid sequences i.e. aaSeqs
+        enable("aaSeqs")
+        output$aaSeqs <- downloadHandler(
+            filename = function() {
+                paste0(input$strainChoice, "_proteinSequences.fasta")
+            },
+            content = function(file) {
+                write.fasta(
+                    as.list(filteredTable()$Amino_Acid_Sequence),
+                    names = filteredTable()$Locus_Tag,
+                    file.out = file
+                    )
+            }
+        )
 
 
     })
