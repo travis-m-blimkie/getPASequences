6fa8676d254be5dac85e5908ae770ed42695fdee
Travis <travis.m.blimkie@gmail.com>
2019-10-24 15:35:13 -0700

    Rearranged order of functions.

--- global.R
+++ global.R
@@ -12,25 +12,6 @@ orthologs_PAO1_LESB58 <- readRDS("data/orthologs_PAO1_LESB58.rds")
 orthologs_PA14_LESB58 <- readRDS("data/orthologs_PA14_LESB58.rds")
 
 
-# Define function for ortholog mapping ------------------------------------
-
-mapOrthosGenerally <- function(inputDF, strain1, strain2) {
-
-  if (strain1 %in% c("PAO1", "PA14") & strain2 %in% c("PAO1", "PA14")) {
-    inner_join(inputDF, orthologs_PAO1_PA14)
-
-  } else if (strain1 %in% c("PAO1", "LESB58") & strain2 %in% c("PAO1", "LESB58")) {
-    inner_join(inputDF, orthologs_PAO1_LESB58)
-
-  } else if (strain1 %in% c("PA14", "LESB58") & strain2 %in% c("PA14", "LESB58")) {
-    inner_join(inputDF, orthologs_PA14_LESB58)
-
-  } else {
-    return(NULL)
-  }
-}
-
-
 # Define function for getting annotations ---------------------------------
 
 retrieveAnnotations <- function(inputDF, strain) {
@@ -43,6 +24,25 @@ retrieveAnnotations <- function(inputDF, strain) {
 
   } else if (strain == "LESB58") {
     inner_join(inputDF, annosLESB58,  by = "Locus Tag")
+
+  } else {
+    return(NULL)
+  }
+}
+
+
+# Define function for ortholog mapping ------------------------------------
+
+mapOrthosGenerally <- function(inputDF, strain1, strain2) {
+
+  if (strain1 %in% c("PAO1", "PA14") & strain2 %in% c("PAO1", "PA14")) {
+    inner_join(inputDF, orthologs_PAO1_PA14)
+
+  } else if (strain1 %in% c("PAO1", "LESB58") & strain2 %in% c("PAO1", "LESB58")) {
+    inner_join(inputDF, orthologs_PAO1_LESB58)
+
+  } else if (strain1 %in% c("PA14", "LESB58") & strain2 %in% c("PA14", "LESB58")) {
+    inner_join(inputDF, orthologs_PA14_LESB58)
 
   } else {
     return(NULL)
