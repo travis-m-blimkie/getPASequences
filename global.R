c175ac47f38988825996375b6dedb4d0b03287b6
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-08-13 14:36:22 -0700

    Removed calls to unused libraries.

--- global.R
+++ global.R
@@ -1,8 +1,6 @@
 
 # Load all required libraries ---------------------------------------------
 
-library(shinythemes)
-library(shinyjs)
 library(seqinr)
 library(DT)
 library(tidyverse)
@@ -11,12 +9,12 @@ library(tidyverse)
 # Load all the data files -------------------------------------------------
 
 # Annotation files
-annosPAO1 <- readRDS("data/annotations_PAO1_107.rds")
-annosPA14 <- readRDS("data/annotations_UCBPP-PA14_109.rds")
+annosPAO1   <- readRDS("data/annotations_PAO1_107.rds")
+annosPA14   <- readRDS("data/annotations_UCBPP-PA14_109.rds")
 annosLESB58 <- readRDS("data/annotations_LESB58_125.rds")
 
 # Ortholog relations
-orthologs_PAO1_PA14 <- readRDS("data/orthologs_PAO1_PA14.rds")
+orthologs_PAO1_PA14   <- readRDS("data/orthologs_PAO1_PA14.rds")
 orthologs_PAO1_LESB58 <- readRDS("data/orthologs_PAO1_LESB58.rds")
 orthologs_PA14_LESB58 <- readRDS("data/orthologs_PA14_LESB58.rds")
 
